010b7bb9dff9f00a922dd2b467b187544dae9e53
root <sjbuy25@gmail.com>
2018-05-16 06:33:51 +0000

    adding to testrepo2

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
-ass testrepo2  {
-    if $environment == 'production' {
-      notify { 'default-message' : 
-              message => " this is the production env"
-            }
-    } else {
-            notify { 'default-message' :
-                 message => "This is not production env"
-            }
-    }  
+class testrepo2  {
+#    if $environment == 'production' {
+#      notify { 'default-message' : 
+#              message => " this is the production env"
+#            }
+#    } else {
+#            notify { 'default-message' :
+#                 message => "This is not production env"
+#            }
+#    }  
 }
